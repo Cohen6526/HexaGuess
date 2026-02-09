6d606d8b9180118718026c4e78bad0eab238c05e
Cohen6526 <cohenteschke@gmail.com>
2026-02-09 12:49:29 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,150 @@
+let colorSample = null; 
+let answers = [];
+let correctColorCode = null;
+let score = 0;
+let total = 0;
+let correctAnswer = null;
+let colorCode = null;
+let flipflop = false;
+let hexes = [];
+let gamemode = "standard";
+
+window.onload = function () {
+    colorSample = document.getElementById("colorSample");
+    document.getElementById("score").innerHTML = "0/0";
+}
+
+function changedifficulty() {
+    if(document.getElementById("switch").checked == false){
+        gamemode = "standard";
+    }else{
+        gamemode = "flipped"; //GET THIS TO WORK!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! MAYBE MAKE A NEW SWITCH!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+}
+
+
+
+function startGame(diff){
+    document.getElementById("easy").style.display = "none";
+    document.getElementById("medium").style.display = "none";
+    document.getElementById("hard").style.display = "none";
+    document.getElementById("gamemodechooser").style.display = "none";
+    document.getElementById("scoreboard").style.display = "block";
+    document.getElementById("colorSample").style.display = "block";
+    
+    answers.push(document.getElementById("0"));
+    answers.push(document.getElementById("1"));
+    if(diff == "medium"){
+    answers.push(document.getElementById("2"));
+    answers.push(document.getElementById("3"));
+    }
+    if(diff == "hard"){
+    answers.push(document.getElementById("2"));
+    answers.push(document.getElementById("3"));
+    answers.push(document.getElementById("4"));
+    answers.push(document.getElementById("5"));
+    answers.push(document.getElementById("6"));
+    answers.push(document.getElementById("7"));
+    }
+    for (let i = 0; i < answers.length; i++){
+        answers[i].style.display = "inline-block";
+    }
+    
+    loadNewQuestion();
+}
+function loadNewQuestion(){
+    colorCode = getRandomHexCode();
+    colorSample.innerHTML = "";
+    if(gamemode == "standard"){
+        colorSample.style.backgroundColor = colorCode;
+        document.getElementById("title").innerHTML = "Guess the hex code for this colour!";
+    }else if(gamemode == "flipped"){
+        colorSample.innerHTML = colorCode;
+        document.getElementById("title").innerHTML = "Guess the colour for this hex code!";
+        document.getElementById("colorSample").style.height = "50px";
+        for (let i = 0; i < answers.length; i++){
+            answers[i].innerHTML = "‎ ";
+        }
+    }
+    
+    correctColorCode = colorCode;
+    for (let x = 0; x < answers.length; x++){
+        hexes.unshift(getRandomHexCode())
+        if(gamemode == "standard"){
+        answers[x].innerHTML = hexes[0];
+        }else if(gamemode == "flipped"){
+            answers[x].style.backgroundColor = hexes[0];
+        }
+    }
+    correctAnswer = Math.floor(Math.random() * (answers.length))
+    if(gamemode == "standard"){
+        answers[correctAnswer].innerHTML = colorCode;
+    }else if(gamemode == "flipped"){
+        answers[correctAnswer].style.backgroundColor = colorCode;
+    }
+    hexes[correctAnswer] = colorCode;
+}
+
+function getRandomHexCode(){
+    let result = [];
+    let hexRef = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+    result.push("#");
+    for (let n = 0; n < 6; n++){
+        result.push(hexRef[Math.floor(Math.random() * 16)]);
+    }
+    return result.join('');
+}
+
+function pop(button){
+    if(flipflop == false){
+    document.getElementById(button).style.animation = "pulse .5s ease-out";
+    flipflop = true;
+    setTimeout(recolour, 250, button);
+    }
+}
+function recolour(button){
+    if(gamemode == "standard"){
+        if(document.getElementById(button).innerHTML == colorCode){
+            document.getElementById(button).style.backgroundColor = "#00FF00";
+            document.getElementById(button).innerHTML = "CORRECT";
+            score++;
+        }
+        else{
+            document.getElementById(button).style.backgroundColor = "#FF0000";
+            document.getElementById(button).innerHTML = "INCORRECT";
+        };
+    }else if(gamemode == "flipped"){
+        if(hexes[button] == colorCode){
+            document.getElementById(button).style.backgroundColor = "#00FF00";
+            document.getElementById(button).innerHTML = "CORRECT";
+            score++;
+        }else{
+            document.getElementById(button).style.backgroundColor = "#FF0000";
+            document.getElementById(button).innerHTML = "INCORRECT";
+        }
+    }
+    setTimeout(disappear, 250, button);
+}
+function disappear(button) {
+    document.getElementById(button).style.animation = "none";
+    setTimeout(anotherquestion, 500, button);
+    
+}
+function anotherquestion(button) {
+    total++;
+    document.getElementById("score").innerHTML = score + "/" + total;
+    document.getElementById(button).style.backgroundColor = "#CFCCD6";
+    flipflop = false;
+    if(total == 10){
+        endGame();
+    }else{
+    loadNewQuestion();
+    }
+}
+function endGame(){
+    for (let i = 0; i < answers.length; i++){
+        answers[i].style.display = "none";
+        colorSample.style.display = "none";
+        document.getElementById("title").innerHTML = "Final Score:";
+    }
+}
