2e4c69aa6ca310f9c72af495e286211ea8c44090
Cohen6526 <cohenteschke@gmail.com>
2026-02-10 14:13:18 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -135,6 +135,7 @@ function anotherquestion(button) {
     document.getElementById("score").innerHTML = score + "/10";
     document.getElementById(button).style.backgroundColor = "#CFCCD6";
     flipflop = false;
+    total++;
     if(total == 10){
         endGame();
     }else{
@@ -148,3 +149,38 @@ function endGame(){
         document.getElementById("title").innerHTML = "Final Score:";
     }
 }//endgame
+
+// load the service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+        console.log('Service Worker registered with scope:', registration.scope);
+      }, function(error) {
+        console.log('Service Worker registration failed:', error);
+      });
+    });
+  }  
+
+  // handle install prompt
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const installButton = document.getElementById('installButton');
+  installButton.style.display = 'block';
+
+  installButton.addEventListener('click', () => {
+    installButton.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+}); 
